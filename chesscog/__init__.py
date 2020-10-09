6426b92d0c003bb24d60e8fa084567bf3f28cb77
Georg Wölflein <georgw7777@gmail.com>
2020-10-09 20:45:06 +0100

    Fix DATA_DIR

--- chesscog/__init__.py
+++ chesscog/__init__.py
@@ -2,6 +2,6 @@ from pathlib import Path
 import os
 
 if "DATA_DIR" in os.environ:
-    DATA_DIR = os.environ["DATA_DIR"]
+    DATA_DIR = Path(os.environ["DATA_DIR"])
 else:
     DATA_DIR = Path(__file__).parent.parent / "data"
