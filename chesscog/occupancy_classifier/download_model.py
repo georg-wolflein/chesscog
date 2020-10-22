42f87408fa6a6e10fa3ff7d297987e2aee012efe
Georg Wölflein <georgw7777@gmail.com>
2020-10-23 00:07:34 +0100

    Fix tests

--- chesscog/occupancy_classifier/download_model.py
+++ chesscog/occupancy_classifier/download_model.py
@@ -1,7 +1,13 @@
 """Script to download the best occupancy classifier."""
 
-from chesscog.utils.io import URI, download_zip_folder_from_google_drive
+import functools
+from chesscog.utils.io import download_zip_folder_from_google_drive
 
-download_zip_folder_from_google_drive("1mLaSIh7KzGeMuyGmhtwIYVVVN6l_eC1k",
-                                      "models://occupancy_classifier",
-                                      show_size=True)
+
+ensure_model = functools.partial(download_zip_folder_from_google_drive,
+                                 "1mLaSIh7KzGeMuyGmhtwIYVVVN6l_eC1k",
+                                 "models://occupancy_classifier")
+
+
+if __name__ == "__main__":
+    ensure_model(show_size=True)
