9540e7a007e663cc7cffabdb2cafe40556fc5f20
Georg Wölflein <georgw7777@gmail.com>
2020-11-06 16:01:14 +0000

    Ensure models

---
+++ tests/classifiers/test_download_models.py
@@ -0,0 +1,15 @@
+import pytest
+import typing
+
+from chesscog.occupancy_classifier.download_model import ensure_model as ensure_occupancy_classifier
+from chesscog.piece_classifier.download_model import ensure_model as ensure_piece_classifier
+from chesscog.utils.io import URI
+
+
+@pytest.mark.parametrize("ensure_model,name", [
+    (ensure_occupancy_classifier, "occupancy_classifier"),
+    (ensure_piece_classifier, "piece_classifier")
+])
+def test_ensure_model(ensure_model: typing.Callable, name: str):
+    ensure_model(show_size=False)
+    assert len(list(URI(f"models://{name}").glob("*.pt"))) > 0
--- tests/occupancy_classifier/test_download_model.py
+++
@@ -1,7 +0,0 @@
-from chesscog.occupancy_classifier.download_model import ensure_model
-from chesscog.utils.io import URI
-
-
-def test_ensure_model():
-    ensure_model(show_size=False)
-    #assert URI("models://occupancy_classifier.pt").exists()
