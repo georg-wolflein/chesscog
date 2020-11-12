4af9d4fa1e46e1c8c5792eb01ce01a87ec6e9462
Georg Wölflein <georgw7777@gmail.com>
2020-11-12 22:31:39 +0000

    Download model from GitHub release

--- chesscog/occupancy_classifier/download_model.py
+++ chesscog/occupancy_classifier/download_model.py
@@ -1,11 +1,11 @@
 """Script to download the best occupancy classifier."""
 
 import functools
-from chesscog.utils.io import download_zip_folder_from_google_drive
+from chesscog.utils.io import download_zip_folder
 
 
-ensure_model = functools.partial(download_zip_folder_from_google_drive,
-                                 "1MUGB_OlcbHFyHh2BwtkXXi6ZBPeEfHtQ",
+ensure_model = functools.partial(download_zip_folder,
+                                 "https://github.com/georgw777/chess-recognition/releases/download/v0.1.0/occupancy_classifier.zip",
                                  "models://occupancy_classifier")
 
 
