50ada339c23575e20245a5d97c0581c0ff19b567
Georg Wölflein <georgw7777@gmail.com>
2020-11-20 18:14:19 +0000

    Update model download links

--- chesscog/occupancy_classifier/download_model.py
+++ chesscog/occupancy_classifier/download_model.py
@@ -5,7 +5,7 @@ from chesscog.utils.io import download_zip_folder
 
 
 ensure_model = functools.partial(download_zip_folder,
-                                 "https://github.com/georgw777/chesscog/releases/download/v0.1.0/occupancy_classifier.zip",
+                                 "https://github.com/georgw777/chesscog/releases/download/0.1.0/occupancy_classifier.zip",
                                  "models://occupancy_classifier")
 
 
