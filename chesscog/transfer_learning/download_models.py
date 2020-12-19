96e03040d84320cab2dda80ac883d055b3b395e2
Georg Wölflein <georgw7777@gmail.com>
2020-12-19 21:09:23 +0100

    Change link for transfer learning models

--- chesscog/transfer_learning/download_models.py
+++ chesscog/transfer_learning/download_models.py
@@ -5,7 +5,7 @@ from chesscog.core.io import download_zip_folder
 
 
 ensure_models = functools.partial(download_zip_folder,
-                                  "https://github.com/georgw777/chesscog/releases/download/0.1.0/transfer_learning_models.zip",
+                                  "https://github.com/georgw777/chesscog/releases/download/0.2.7/transfer_learning_models.zip",
                                   "models://transfer_learning")
 
 
