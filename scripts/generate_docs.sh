05984c3936249b3a34cf3bc05ad7f327a3ed2d60
Georg Wölflein <georgw7777@gmail.com>
2020-12-28 17:52:00 +0100

    Remove PDF docs

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -8,11 +8,11 @@ cd $SCRIPTPATH/..
 mkdir -p docs
 poetry run pdoc3 --html --force --output-dir docs/html chesscog
 
-poetry run pdoc3 --pdf --config='docformat="google"' chesscog > docs.md
-# fix headings
-awk '{gsub(/-----=/,":\n")}1' docs.md > tmp.md && mv tmp.md docs.md
-pandoc  --metadata=title:"Documentation of the chesscog package" \
-        --from=markdown+abbreviations+tex_math_single_backslash  \
-        --pdf-engine=xelatex \
-        --toc --toc-depth=4 --output=docs/chesscog.pdf  docs.md
-rm docs.md
+# poetry run pdoc3 --pdf --config='docformat="google"' chesscog > docs.md
+# # fix headings
+# awk '{gsub(/-----=/,":\n")}1' docs.md > tmp.md && mv tmp.md docs.md
+# pandoc  --metadata=title:"Documentation of the chesscog package" \
+#         --from=markdown+abbreviations+tex_math_single_backslash  \
+#         --pdf-engine=xelatex \
+#         --toc --toc-depth=4 --output=docs/chesscog.pdf  docs.md
+# rm docs.md
