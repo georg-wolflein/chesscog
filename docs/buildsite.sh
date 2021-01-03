945a47b40e975500ab1c10a25bd2a64b40e69877
Georg Wölflein <georgw7777@gmail.com>
2021-01-03 22:04:34 +0100

    CI

--- docs/buildsite.sh
+++ docs/buildsite.sh
@@ -2,7 +2,7 @@
 set -x
 
 apt-get update
-apt-get -y install git rsync libgl1-mesa-glx build-essential
+apt-get -y install git rsync libgl1-mesa-glx libglib2.0-0 build-essential
 
 pwd ls -lah
 export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
