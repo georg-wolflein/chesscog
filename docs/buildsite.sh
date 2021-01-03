a02ab129fb9a8397a4231d0bf9c9d1da5eea0ac2
Georg Wölflein <georgw7777@gmail.com>
2021-01-03 21:57:32 +0100

    CI

--- docs/buildsite.sh
+++ docs/buildsite.sh
@@ -13,8 +13,8 @@ export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
  
 # Python Sphinx, configured with source/conf.py
 # See https://www.sphinx-doc.org/
-make clean
-make html
+poetry run make clean
+poetry run make html
 
 #######################
 # Update GitHub Pages #
